ab8229a8ce3688a94a94d7a8eb298aa74e6fe82e
plaha303 <111688671+plaha303@users.noreply.github.com>
2023-11-29 09:39:21 +0200

    changing the code structure

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,4 @@
-from datetime import date
 from sqlalchemy.orm import sessionmaker
-from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,23 +6,6 @@ from src.database.db import engine
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-class ContactRequest(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    phone_number: str
-    birthday: date
-
-
-class ContactResponse(BaseModel):
-    id: int
-    first_name: str
-    last_name: str
-    email: str
-    phone_number: str
-    birthday: date
 
 
 class Contact(Base):
